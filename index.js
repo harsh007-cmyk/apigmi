5af37810cf744389646f43e35e08706235a11142
Harsh P <harshpmpm07@gmail.com>
2022-11-01 15:17:33 +0530

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const cors=require("cors");
+const app=require('express')();
+app.use(cors())
+const PORT=process.env.PORT||3000;
+const apiData=require("./data.json")
+app.listen(PORT,
+    ()=>console.log(`run on http://localhost:${PORT}`)
+        );
+app.get('/',(req,res)=>{
+    res.send(apiData)
+})
